8ad16f32e0e7dc91060b5b399d28b7c432ef4f52
ShivamSh002 <shivamsharma98879@gmail.com>
2024-02-28 19:52:51 +0530

    final commit

--- x-stopwatch/src/App.js
+++ x-stopwatch/src/App.js
@@ -1,67 +1,41 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 const App = () => {
-  const [time, setTime] = useState({
-    sec: 0,
-    min: 0,
-    hr: 0,
-  });
-  const [pauseTime, setPauseTime] = useState(true);
-  const [intervalId, setIntervalId] = useState();
+  const [isRunning, setIsRunning] = useState(false);
+  const [elapseddTime, setElapseddTime] = useState(0);
 
-  const updateTimer = () => {
-    setTime((prev) => {
-      let newTime = { ...prev };
-      if (newTime.sec < 59) {
-        newTime.sec += 1;
-      } else {
-        newTime.min += 1;
-        newTime.sec = 0;
-      }
-
-      if (newTime.min === 60) {
-        newTime.min = 0;
-        newTime.hr += 1;
-      }
-
-      return newTime;
-    });
-  };
-
-  const handleTime = () => {
-    setPauseTime(!pauseTime);
-    if (!intervalId) {
-      let id = setInterval(updateTimer, 1000);
-      setIntervalId(id);
+  useEffect(() => {
+    let intervalId;
+    if (isRunning) {
+      intervalId = setInterval(() => {
+        setElapseddTime((prev) => prev + 1);
+      }, 1000);
     } else {
       clearInterval(intervalId);
-      setIntervalId("");
     }
+    return () => clearInterval(intervalId);
+  }, [isRunning]);
+
+  const formatTime = (time) => {
+    const minutes = Math.floor(time / 60);
+    const seconds = time % 60;
+    return `${minutes}:${seconds < 10 ? "0" : ""}${seconds}`;
   };
 
   const reset = () => {
-    clearInterval(intervalId);
-    setTime({
-      sec: 0,
-      min: 0,
-      hr: 0,
-    });
+    setElapseddTime(0);
+    setIsRunning(false);
+  };
+
+  const startStop = () => {
+    setIsRunning(!isRunning);
   };
 
   return (
     <div>
       {" "}
-      <h1> StopWatch </h1>{" "}
-      <h2>
-        {" "}
-        {`${time.hr < 10 ? 0 : ""}${time.hr} : ${time.min < 10 ? 0 : ""}${
-          time.min
-        } : ${time.sec < 10 ? 0 : ""}${time.sec}`}{" "}
-      </h2>{" "}
-      <button on onClick={handleTime}>
-        {" "}
-        {pauseTime ? "Start" : "Pause"}{" "}
-      </button>{" "}
+      <h1> StopWatch </h1> <p> Time: {formatTime(elapseddTime)} </p>{" "}
+      <button onClick={startStop}> {isRunning ? "Stop" : "Start"} </button>{" "}
       <button onClick={reset}> Reset </button>{" "}
     </div>
   );
